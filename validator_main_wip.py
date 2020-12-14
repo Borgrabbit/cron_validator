6239697f253d1b031fa2b747a3e0595ad052dc3b
borgrabbit <ggulto2@gmail.com>
2020-12-14 21:38:24 +0900

    feat: added day,month dictionary for separate module

--- validator_main_wip.py
+++ validator_main_wip.py
@@ -8,6 +8,31 @@ class FormatException(Exception):
 class CronValidator(object):
     _expression = ''
 
+    _days_map = {
+        'SUN': 0,
+        'MON': 1,
+        'TUE': 2,
+        'WED': 3,
+        'THU': 4,
+        'FRI': 5,
+        'SAT': 6
+    }
+
+    _months_map = {
+        'JAN': 1,
+        'FEB': 2,
+        'MAR': 3,
+        'APR': 4,
+        'MAY': 5,
+        'JUN': 6,
+        'JUL': 7,
+        'AUG': 8,
+        'SEP': 9,
+        'OCT': 10,
+        'NOV': 11,
+        'DEC': 12
+    }
+
     _cron_days = {
         0: 'SUN',
         1: 'MON',
